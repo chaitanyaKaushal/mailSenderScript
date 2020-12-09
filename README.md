de06cc3dc7f8869c1f652146128d2ceb0ad674d5
chaitanyaKaushal <ckaushal2028@gmail.com>
2020-12-09 14:02:38 +0530

    Updated for the last commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 A python script to send mails from your gmail, outlook or other mail services.
+
+While running the script for gmail services, you need to keep in mind two points:-
+a) if you have 2-step verification enabled and you do not want to disable it, then you should :
+i) go to your Google Account -> Signing in to Google -> App Passwords -> select the app and device you want to allow -> click generate.
+ii) do the next needful things
+b) disable the 2 step verification, then go to
+i) google account -> security -> less secure apps -> give the allowance
+
+Run your script and you are good to go !
--- emailService.py
+++ emailService.py
@@ -1,16 +1,14 @@
-import smtplib as sm
+# -------------------------------- Refer to README first before copying and pasting my script----------------------------------------------
 # email would be sent to any machine having smtp,esmtp demon client installed
+import smtplib as sm
 # smtp is an email transfer protocol
-# ----------
+
 # server has smtp ssl , smtp server-address and port no
 server = sm.SMTP_SSL("smtp.gmail.com", 465)  # to check settings for gmail
 # smtp.gmail.com is the smtp server address for gmail service
-server.login("your mail comes here", "your password comes here")
-# sender , reciever, message, subject not considered
-server.sendmail("sender mail",
-                "reciever mail", "messsage")
-server.quit()
 
-# For SMTP authentication to be successful ->
-# if 2 step verification is available then generate an app password in your google account settings
-# if you disable 2 step verification, you should give access to Less Secure mail in your google account settings
+server.login("your mail id comes here", "your password comes here")
+# sender , reciever, message, subject not considered
+server.sendmail("sender mail id",
+                "reciever mail id", "messsage")
+server.quit()
