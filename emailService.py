c635a5c1f12faca4fe18212be10aafabddb645e2
chaitanyaKaushal <ckaushal2028@gmail.com>
2020-12-09 13:48:56 +0530

    Script Modified

--- emailService.py
+++ emailService.py
@@ -5,9 +5,12 @@ import smtplib as sm
 # server has smtp ssl , smtp server-address and port no
 server = sm.SMTP_SSL("smtp.gmail.com", 465)  # to check settings for gmail
 # smtp.gmail.com is the smtp server address for gmail service
-server.login("ckaushal2028@gmail.com", "buttercup@@34")
+server.login("your mail comes here", "your password comes here")
 # sender , reciever, message, subject not considered
-server.sendmail("ckaushal2028@gmail.com",
-                "ckaushal_be18@thapar.edu", "Hello ! How are you")
+server.sendmail("sender mail",
+                "reciever mail", "messsage")
 server.quit()
-# APP PASSWORD -> mbakyltbloqbxpqx
+
+# For SMTP authentication to be successful ->
+# if 2 step verification is available then generate an app password in your google account settings
+# if you disable 2 step verification, you should give access to Less Secure mail in your google account settings
